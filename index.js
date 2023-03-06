6df4b26e76968e6446b98ed7bc366c45b96db793
luiggy <luiggylml@gmail.com>
2023-03-05 19:10:57 -0500

    feat: Start project backend

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 4001;
+
+app.use("/api/user", userRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
